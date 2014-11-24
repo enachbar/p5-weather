376f081a17844acdbb202dd2a8c6ed56b96c194d
Emily Nachbar <enachbar@engr-134-21.eastern.ewu.edu>
2014-11-24 14:33:04 -0800

    Set up

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+// Docs at http://simpleweatherjs.com
+$(document).ready(function() {
+  $.simpleWeather({
+    location: 'Austin, TX',
+    woeid: '',
+    unit: 'f',
+    
+    //If can ge weather
+    success: function(weather) {
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      
+      
+
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+    
+
+    //PUT DISPLAY
+      $("#weather").html(html);
+    },
+
+    //if cannot get weather
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+});
