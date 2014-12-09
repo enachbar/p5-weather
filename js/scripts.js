d86669b5fe0c65a1d14c78e2e3c9895d5b7c9cd9
Emily Nachbar <enachbar@live.com>
2014-12-08 21:50:58 -0800

    styles

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ if ("geolocation" in navigator) {
 /* Locator */
 $('.js-geolocation').on('click', function() {
   navigator.geolocation.getCurrentPosition(function(position) {
-    loadWeather(position.coords.latitude+','+position.coords.longitude); //load weather using your lat/lng coordinates
+    loadWeather(position.coords.latitude+','+position.coords.longitude); 
   });
 });
 
@@ -26,16 +26,16 @@ $(document).ready(function() {
   loadWeather('San Francisco',''); 
 });
 
-function loadWeather(location, woeid) {
+function loadWeather(location) {
   $.simpleWeather({
     location: location,
-    woeid: woeid,
+    
     unit: 'f',
     success: function(weather) {
       html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      html += '<ul><li>'+weather.city+', '+weather.region+','+weather.alt.temp+'&deg;C</li> <br>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li> <br>';
       html += '<li class="currently">'+weather.currently+'</li>';
-      html += '<li>'+weather.alt.temp+'&deg;C</li></ul>';  
+      html += '<br><li>'+weather.alt.temp+'&deg;C</li></ul>';  
       
       $("#weather").html(html);
     },
